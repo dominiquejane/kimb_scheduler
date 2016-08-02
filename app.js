c5e10014da3f13d50be15fd87597fe5120bb5eac
Dominique <deejay.whitegrass@gmail.com>
2016-08-02 09:57:55 -0500

    pulling from github

--- app.js
+++ app.js
@@ -1,195 +1,290 @@
 $(document).ready(function() {
 
-    events_array= [
-			        {
-			            title: "",
-			            start: '2016-07-09 06:15:00',
-			            end: '2016-07-09 06:30:00',
-			            resourceId:'e',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "F",
-									procedure: "Lap-band",
-					        color: "grey",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 06:15:00',
-			            end: '2016-07-09 06:30:00',
-			            resourceId:'b',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "F",
-									procedure: "Lap-band",
-					        color: "gold",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 06:15:00',
-			            end: '2016-07-09 06:30:00',
-			            resourceId:'d',
-			            doctor_name: "Kim",
-									patient_name: "S. Stefanopoulis",
-									icon: 'icons/heart-checkup.png',
-									office: "F",
-									procedure: "Lap-band",
-					        color: "green",
-			        },			        
-			        {
-			            title: "",
-			            start: '2016-07-09 09:00:00',
-			            end: '2016-07-09 09:30:00',
-			            resourceId:'c',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "B",
-									procedure: "Lap-band",
-					        color: "red",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 09:00:00',
-			            end: '2016-07-09 09:30:00',
-			            resourceId:'d',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "B",
-									procedure: "Lap-band",
-					        color: "green",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 07:00:00',
-			            end: '2016-07-09 07:15:00',
-			            resourceId:'e',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "F",
-									procedure: "Lap-band",
-					        color: "orange",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 06:00:00',
-			            end: '2016-07-09 06:30:00',
-			            resourceId:'a',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "F",
-									procedure: "Lap-band",
-					        color: "yellow",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 06:00:00',
-			            end: '2016-07-09 06:30:00',
-			            resourceId:'c',
-			            doctor_name: "Kim",
-									patient_name: "S. Dean",
-									icon: 'icons/heart-checkup.png',
-									office: "B",
-									procedure: "Lap-band",
-					        color: "royalblue",
-			        },
-			        {
-			            title: "",
-			            start: '2016-07-09 06:45:00',
-			            end: '2016-07-09 07:00:00',
-			            resourceId: 'e',
-			            doctor_name: "Lee",
-									patient_name: "D. Sean",
-									icon: "",
-									office: "B",
-									procedure: "Braces",
-					        color: "mediumorchid",
-			        } ]
+	
+
+
+	// var events_array = [{
+ //        title: "",
+ //        start: '2016-07-14 06:15:00',
+ //        end: '2016-07-14 06:30:00',
+ //        resourceId:'e',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "grey",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 06:15:00',
+ //        end: '2016-07-14 06:30:00',
+ //        resourceId:'b',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "gold",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 06:15:00',
+ //        end: '2016-07-14 06:30:00',
+ //        resourceId:'b',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "gold",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-15 06:15:00',
+ //        end: '2016-07-15 06:30:00',
+ //        resourceId:'d',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "green",
+ //    },              
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 14:00:00',
+ //        end: '2016-07-14 14:30:00',
+ //        resourceId:'c',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "B",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "red",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 14:00:00',
+ //        end: '2016-07-14 14:30:00',
+ //        resourceId:'d',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "B",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "green",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 07:00:00',
+ //        end: '2016-07-14 07:15:00',
+ //        resourceId:'e',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "orange",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 06:00:00',
+ //        end: '2016-07-14 06:30:00',
+ //        resourceId:'a',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "F",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "yellow",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 06:00:00',
+ //        end: '2016-07-14 06:30:00',
+ //        resourceId:'c',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Kim",
+ //        reason: "",
+ //        notes: "",
+ //        icon: 'icons/heart-checkup.png',
+ //        office: "B",
+ //        procedure: "Lap-band",
+ //        status: "",
+ //        color: "royalblue",
+ //    },
+ //    {
+ //        title: "",
+ //        start: '2016-07-14 06:45:00',
+ //        end: '2016-07-14 07:00:00',
+ //        resourceId: 'e',
+ //        patient_firstName: "Sean",
+ //        patient_lastName: "Dean",
+ //        doctor_firstName: "John",
+ //        doctor_lastName: "Lee",
+ //        reason: "",
+ //        notes: "",
+ //        icon: "",
+ //        office: "B",
+ //        procedure: "Braces",
+ //        status: "",
+ //        color: "mediumorchid",
+ //    }
+ //  ];
+
+	// var events_array = [{
+	//   "appointments": [
+	//     {
+	//     	"start" : "",
+	//     	"end" : "",
+	//       "id": 001,
+	//       "patient_id": 101,
+	//       "rescheduled_at": "",
+	//       "staff_id": 010,
+	//       "status": "exercitation ut enim Duis",
+	//       "time": "dolor",
+	//     },
+	//     {
+	//     	"start" : "",
+	//     	"end" : "",
+	//       "id": 002,
+	//       "patient_id": 102,
+	//       "rescheduled_at": "",
+	//       "staff_id": 020,
+	//       "status": "exercitation ut enim Duis",
+	//       "time": "dolor",
+	//     },
+	//     {
+	//     	"start" : "",
+	//     	"end" : "",
+	//       "id": 003,
+	//       "patient_id": 103,
+	//       "rescheduled_at": "",
+	//       "staff_id": 030,
+	//       "status": "exercitation ut enim Duis",
+	//       "time": "dolor",
+	//     },
+	//     {
+	//     	"start" : "",
+	//     	"end" : "",
+	//       "id": 004,
+	//       "patient_id": 104,
+	//       "rescheduled_at": "",
+	//       "staff_id": 040,
+	//       "status": "exercitation ut enim Duis",
+	//       "time": "dolor",
+	//     },
+	//   ],
+	//   "classes": [
+	//     {
+	//       "attendees": ["parvati", "amanda", "cerie"],
+	//       "start": "",
+	//       "end" : "",
+	//       "id": 2.1188895e+07,
+	//       "staff_id": 030,
+	//       "time": "Duis dolore",
+	//       "title": "anim",
+	//     },
+	//     {
+	//       "attendees": ["eliza", "ami", "fairplay", "kathy", "mikey", "chet"],
+	//       "start": "",
+	//       "end" : "",
+	//       "id": 2.1188895e+07,
+	//       "staff_id": 030,
+	//       "time": "Duis dolore",
+	//       "title": "anim",
+	//     },
+	//     {
+	//       "attendees": ["ozzy", "james", "jason", "eric"],
+	//       "start": "",
+	//       "end" : "",
+	//       "id": 2.1188895e+07,
+	//       "staff_id": 020,
+	//       "time": "Duis dolore",
+	//       "title": "anim",
+	//     },
+	//   ],
+	//   "time-blocks": [
+	//     {
+	//       "staff_id": "",
+	//       "client_id": "", //location
+	//       "start": "",
+	//       "end" : "",
+	//     }
+	//   ]
+	// }];
 
 
 
-    $('#calendar').fullCalendar({
+	// $('#calendar').fullCalendar({
 
-      schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-      defaultView: 'agendaDay',
-      resources: [
-      	{ id: 'a', title: 'O1' },
-        { id: 'b', title: 'O2' },
-        { id: 'c', title: 'O3' },
-        { id: 'd', title: 'P1' },
-        { id: 'e', title: 'P2' }
-      ],
-      // resources: [
-      // 	{ id: 'a', title: 'O1' },
-      //   { id: 'b', title: 'O2' },
-      //   { id: 'c', title: 'O3' },
-      //   { id: 'd', title: 'P1' },
-      //   { id: 'e', title: 'P1' },
-      //   { id: 'f', title: 'P1' },
-      //   { id: 'g', title: 'P1' },
-      //   { id: 'h', title: 'P1' },
-      //   { id: 'i', title: 'P1' },
-      //   { id: 'j', title: 'P1' },
-      //   { id: 'k', title: 'P1' },
-      //   { id: 'l', title: 'P1' },
-      //   { id: 'm', title: 'P1' },
-      //   { id: 'n', title: 'P2' }
-      // ],
-      slotDuration: {minutes: 15}, //required
-      header: {
-      	left: 'prev, next today',
-      	center: 'title',
-      	right: 'month, agendaWeek, agendaDay'
-      },
-      events: events_array,
-  
-	    eventRender: function(event, element) {
-
-	    	//get the # of rooms so css classes will be applied accordingly
-	    	var res = $('#calendar').fullCalendar('getResources').length; 
-	    	
-	    	event.patient_name = event.patient_name.toUpperCase();
-	    	
-	    	//give appts the room name if in week agenda
-	    	var view = $('#calendar').fullCalendar('getView'); 
-	    	if (view.name === "agendaWeek") { 
-
-	    		switch(event.resourceId){
-	    			case "a" :
-	    				event.title = "O1";
-	    				break;
-    				case "b" :
-    					event.title = "O2";
-    					break;
-    				case "c" :
-    					event.title = "O3";
-    					break;
-    				case "d" :
-    					event.title = "P1";
-    					break;
-    				case "e" :
-    					event.title = "P2";
-    					break;   					    					
-	    		}
-
-	    		element.html("<div class='col-week'>" + event.title + "</div>")
-
-	    	}else if(res > 6) { //html for calendars with less than 6 rooms
-    			element.html("<div><div class='col-1-day-7'><div class='main-day'><div class='name-day'>" + event.patient_name + "</div><div class='procedure-day'>" + event.procedure + "</div></div><div class='doctor-day'>" + event.doctor_name + "</div></div><div class='col-2-day-7'><div class='office-day-7'>" + event.office + "</div><img class='icon-day-7' src='icons/heart-checkup.png' alt='img' ></div></div>")
-	    	}else { //html for calendars with more than 6 rooms
-	    		element.html("<div><div class='col-1-day'><div class='main-day'><div class='name-day'>" + event.patient_name + "</div><div class='procedure-day'>" + event.procedure + "</div></div><div class='doctor-day'>" + event.doctor_name + "</div></div><div class='col-2-day'><div class='office-day'>" + event.office + "</div><img class='icon-day' src='icons/heart-checkup.png' alt='img' ></div></div>")
-	    	}
-
-	    },
+ //    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+ //    defaultView: 'agendaDay',
+    
+ //    slotDuration: {minutes: 15}, //required
+ //    //events: events_array,
+ //    header: {
+ //        left: 'title',
+ //        center: 'prev,next today',
+ //        right:  'month,agendaWeek,agendaDay'
+ //    },
+ //    eventLimit: true,
+ //    events: calendarData.appointments,
+ //    viewRender: function() {
+ //    	var viewNow = $('#calendar').fullCalendar('getView');
+ //    	if(viewNow.type === "agendaDay") {
+ //    		$('tbody.fc-body').css('display', 'none');
+ //    		// $('tbody.fc-body').before('<table id="dayAgenda-display"><thead id="dayAgenda-display-head"><tr><td>what</td></tr></thead><tbody id="dayAgenda-display-body"></tbody></table>')
+ //    	}
+ //    },
 
 
-    })
-
-});
+ //   })
 
 
 
+
+})
